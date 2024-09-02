690b312de37124390bb64e83a5164dbe47bd27b7
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-09-02 18:13:17 +0200

    Hotels component added to display recommended hotels

---
+++ src/view-trip/components/Hotels.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import HotelCardItem from './HotelCardItem';
+
+
+function Hotels({ trip }) {
+  return (
+    <div>
+      <h2 className='text-2xl font-bold mt-5'>Hôtels recommandés</h2>
+
+      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-3 gap-4'>
+        {trip.hotels && trip.hotels.length > 0 ? (
+          trip.hotels.map((hotel, index) => (
+            <HotelCardItem key={index} hotel={hotel} />
+          ))
+        ) : (
+          <p>Aucun hôtel disponible pour ce voyage.</p>
+        )}
+      </div>
+    </div>
+  );
+}
+
+Hotels.propTypes = {
+  trip: PropTypes.shape({
+    hotels: PropTypes.arrayOf(
+      PropTypes.shape({
+        hotelName: PropTypes.string.isRequired,
+        hotelAddress: PropTypes.string.isRequired,
+        price: PropTypes.string.isRequired,
+        rating: PropTypes.string,
+        description: PropTypes.string,
+      })
+    ).isRequired,
+  }).isRequired,
+};
+
+export default Hotels;
