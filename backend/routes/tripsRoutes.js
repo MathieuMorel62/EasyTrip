2219d34904815c949383927049f1e2c126e29811
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-09-09 16:41:51 +0200

    Add delete functionality for trips

--- backend/routes/tripsRoutes.js
+++ backend/routes/tripsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { createTrip, getTrips, getTripById } from '../controllers/tripsController.js';
+import { createTrip, getTrips, getTripById, deleteTripById } from '../controllers/tripsController.js';
 import { verifyToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/', verifyToken, createTrip);
 router.get('/', verifyToken, getTrips);
 router.get('/:tripId', verifyToken, getTripById);
+router.delete('/:tripId', verifyToken, deleteTripById);
 
 export default router;
