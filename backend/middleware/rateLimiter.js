1c3a42647bd0914c75849cd45e5f5f9acf54d8dd
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-09-09 22:02:57 +0200

    Update rate limiter configuration to allow 10 requests per 15 minutes instead of 5

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -1,11 +1,11 @@
-import rateLimit from 'express-rate-limit';
-
+import rateLimit from "express-rate-limit";
 
 // Configuration du limiteur de taux pour les requêtes d'authentification
 export const authLimiter = rateLimit({
   // Définit la fenêtre de temps pour le limiteur de taux à 15 minutes
   windowMs: 15 * 60 * 1000,
-  // Limite chaque IP à 5 requêtes par fenêtre de 15 minutes
-  max: 5,
-  message: 'Trop de tentatives de connexion depuis cette IP, veuillez réessayer après 15 minutes.'
+  // Limite chaque IP à 10 requêtes par fenêtre de 15 minutes
+  max: 10,
+  message:
+    "Trop de tentatives de connexion depuis cette IP, veuillez réessayer après 15 minutes.",
 });
