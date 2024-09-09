1ce720db96378225c437d111077e28350aa1a399
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-09-09 22:07:15 +0200

    Update authRoutes.js to include routes for user deletion and update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,17 +1,25 @@
-import express from 'express';
-import { signup, login, googleLogin } from '../controllers/authController.js';
-import { authLimiter } from '../middleware/rateLimiter.js';
-import { validateSignup } from '../utils/validators.js';
+import express from "express";
+import {
+  signup,
+  login,
+  googleLogin,
+  updateUser,
+  deleteUser,
+} from "../controllers/authController.js";
+import { authLimiter } from "../middleware/rateLimiter.js";
+import { validateSignup } from "../utils/validators.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 // Création d'un routeur Express
 const router = express.Router();
 
-// Définition des routes pour l'authentification avec limiteur de taux et validation des données d'inscription
-router.post('/signup', authLimiter, validateSignup, signup);
-router.post('/login', authLimiter, login);
-router.post('/google-login', googleLogin);
+// Routes pour l'authentification
+router.post("/signup", authLimiter, validateSignup, signup);
+router.post("/login", authLimiter, login);
+router.post("/google-login", googleLogin);
 
+// Routes pour la gestion des utilisateurs
+router.put("/update", authLimiter, verifyToken, updateUser);
+router.delete("/delete", authLimiter, verifyToken, deleteUser);
 
 export default router;
-
-
