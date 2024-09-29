4ed5329f53082e118d6c740b984866b12cd44016
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-09-29 12:05:23 +0200

    Add missing import statement in LoginDialog component

--- src/components/custom/__tests__/LoginDialog.test.jsx
+++ src/components/custom/__tests__/LoginDialog.test.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import { render, screen, fireEvent } from "@testing-library/react";
 import { GoogleOAuthProvider } from "@react-oauth/google";
