a5534e2c10da5f69eefa1dc1a13aa98cb762626f
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-10-12 14:49:42 +0200

    Update MySQL connection configuration and tests

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,25 +1,28 @@
-import mysql from 'mysql2';
-import dotenv from 'dotenv';
+import mysql from 'mysql2/promise';
 
-// Charge les variables depuis le fichier .env
-dotenv.config();
-
-// Crée une connexion à la base de données MySQL
-const db = mysql.createConnection({
-  //host: process.env.DB_HOST,
-  host: process.env.DB_HOST === '127.0.0.1' ? 'host.docker.internal' : process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-// Etablit la connexion à la base de données
-db.connect((err) => {
-  if (err) {
-    console.error('Erreur de connexion à MySQL:', err);
-    throw err;
-  }
-  console.log('Connexion à MySQL réussie...');
-});
+const db = {
+  connection: null,
+  connect: async () => {
+    try {
+      db.connection = await mysql.createConnection({
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      });
+      await db.connection.connect();
+      console.log('Connecté à la base de données MySQL');
+    } catch (err) {
+      console.error('Erreur de connexion à MySQL:', err);
+      throw err;
+    }
+  },
+  query: async (sql, params) => {
+    if (!db.connection) {
+      await db.connect();
+    }
+    return db.connection.query(sql, params);
+  },
+};
 
 export default db;
