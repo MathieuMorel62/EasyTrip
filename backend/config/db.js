1801516a38cfc4f455d657744d49b8ef2eae4add
Mathieu <113856302+MathieuMorel62@users.noreply.github.com>
2024-11-10 21:16:09 +0100

    Refactoring MySQL database configuration

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,28 +1,24 @@
-import mysql from 'mysql2/promise';
+import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-const db = {
-  connection: null,
-  connect: async () => {
-    try {
-      db.connection = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-      });
-      await db.connection.connect();
-      console.log('Connecté à la base de données MySQL');
-    } catch (err) {
-      console.error('Erreur de connexion à MySQL:', err);
-      throw err;
-    }
-  },
-  query: async (sql, params) => {
-    if (!db.connection) {
-      await db.connect();
-    }
-    return db.connection.query(sql, params);
-  },
-};
+// Charge les variables depuis le fichier .env
+dotenv.config();
+
+// Crée une connexion à la base de données MySQL
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+// Etablit la connexion à la base de données
+db.connect((err) => {
+  if (err) {
+    console.error('Erreur de connexion à MySQL:', err);
+    throw err;
+  }
+  console.log('Connexion à MySQL réussie...');
+});
 
 export default db;
